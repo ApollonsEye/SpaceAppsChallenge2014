fff6e1c1b05a6dd09fc6e3c76f53a03c722e2fc8
ishi-daishi <yellowmarchbank-tr@yahoo.co.jp>
2014-04-29 23:58:04 +0900

    ファイル整理

--- Moverio/Apollon's eye 0.75/gen/ishi_daishi/jp/ne/biglobe/www7b/apollonseye/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ishi_daishi.jp.ne.biglobe.www7b.apollonseye;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
